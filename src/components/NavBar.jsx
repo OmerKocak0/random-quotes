42084bd84bd1012e581e730f828803d4d25ce29c
Ömer Koçak <kocakomer0707@gmail.com>
2026-05-10 22:28:12 +0300

    H6 component Fixed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,12 +8,12 @@ export function NavBar() {
     <nav className="flex justify-between w-full sticky p-3 top-0 z-50 bg-slate-200/70 dark:bg-slate-950/70 backdrop-blur-md border-b border-slate-300/50 dark:border-slate-800 transition-colors duration-300">
       <ul className="flex gap-4 pt-2 pl-4 ">
         <li>
-          <H6 element="nav">
+          <H6 element="span">
             <Link href="/">Main Page</Link>
           </H6>
         </li>
         <li>
-          <H6 element="nav">
+          <H6 element="span">
             <Link href="/user/quotes/liked">Liked Quotes</Link>
           </H6>
         </li>
